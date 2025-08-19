14e20c93a54939fd67b3a2b7dcc8648a4069b193
meanderio <acahn@meanderings.io>
2025-08-18 20:18:38 -0500

    update: using existing data points as centroid starts

--- k-means/KMeans.py
+++ k-means/KMeans.py
@@ -1,33 +1,28 @@
-import numpy as np
+import numpy   as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 
 class KMeans:
     
-    def __init__(self, n_clusters=3, max_iter=1_000):
+    def __init__(self, n_clusters=3, max_iter=1_000, plot=False):
         self.n_clusters = n_clusters
         self.max_iter   = max_iter
         self.centers    = None
+        self.plot       = plot
 
     def euclidian(self, x, y):
         return np.sqrt(np.sum((x - y) ** 2))
     
-    def create_centers(self, X):
+    def initialize_centers(self, X):
         n_samples, n_features = X.shape
-        self.centers = np.zeros(shape=(self.n_clusters, n_features))
-        # create starter centers
-        mins, maxs = np.min(X, axis=0), np.max(X, axis=0)
-        bounds = [[lb, ub] for lb, ub in zip(mins, maxs)]
-        for j in range(self.n_clusters):
-            center = np.zeros(n_features)
-            for i, bound in enumerate(bounds):
-                xi_min, xi_max = bound
-                center[i] = np.random.uniform(low=xi_min, high=xi_max)
-            self.centers[j,:] = center
-
-    def fit(self, X, y):
+        center_idxs = np.random.choice([i for i in range(n_samples)], size=self.n_clusters, replace=False)
+        self.centers = X[center_idxs]
+    
+    def fit(self, X):
         n_samples, n_features = X.shape
         # initialize random centers
-        self.create_centers(X)
+        self.initialize_centers(X)
 
         # until max iter or convergence
         for _ in range(self.max_iter):
@@ -40,6 +35,8 @@ class KMeans:
             for i in range(self.n_clusters):
                 new_centers[i] = np.mean(X[center_map == i], axis=0)
 
+            if self.plot: self.plot_step(X, center_map, new_centers)
+            
             # check if centers have shifted ( todo )
             if np.allclose(self.centers, new_centers): 
                 self.centers = new_centers
@@ -48,9 +45,29 @@ class KMeans:
             # update centers
             self.centers = new_centers
 
+    def plot_step(self, X, y, centers):
+        sns.scatterplot(
+            x = [x[0] for x in X],
+            y = [x[1] for x in X],
+            hue     = y,
+            palette = "deep",
+            legend  = None
+        )
+        sns.scatterplot(
+            x = [x[0] for x in centers],
+            y = [x[1] for x in centers],
+            color=".1", marker="+",
+            palette = "deep",
+            legend  = None
+        )
+
+        plt.xlabel("x1")
+        plt.ylabel("x1")
+        plt.show()
+
     def predict(self, X):
         n_samples, n_features = X.shape
         y_pred = np.zeros(n_samples)
         for i in range(n_samples):
             y_pred[i] = np.argmin([self.euclidian(X[i], c) for c in self.centers])
-        return y_pred
+        return y_pred, self.centers
--- k-means/main.py
+++ k-means/main.py
@@ -1,4 +1,5 @@
-import numpy as np
+import numpy   as np
+import seaborn as sns
 
 from matplotlib              import pyplot as plt
 from sklearn.datasets        import make_blobs, load_iris
@@ -9,25 +10,42 @@ from sklearn.preprocessing   import scale
 from sklearn.cluster import KMeans as SKKM
 from KMeans          import KMeans as ACKM
 
-import plotly.express       as px
-import seaborn              as sns
-import plotly.graph_objects as go
+def plot_step(X, y, centers):
+        sns.scatterplot(
+            x = [x[0] for x in X],
+            y = [x[1] for x in X],
+            hue     = y,
+            palette = "deep",
+            legend  = None
+        )
+        sns.scatterplot(
+            x = [x[0] for x in centers],
+            y = [x[1] for x in centers],
+            color=".1", marker="+",
+            palette = "deep",
+            legend  = None
+        )
+
+        plt.title("Predictions")
+        plt.xlabel("x1")
+        plt.ylabel("x2")
+        plt.show()
 
 def main():
-    n_samples  = 2_000
-    n_features = 6
-    n_centers  = 3
-    max_iter   = 2_000
+    n_samples   = 1_500
+    n_features  = 4
+    n_centers   = 4
+    max_iter    = 2_000
+    cluster_std = 1.2
 
     X, y, centers = make_blobs(
         n_samples      = n_samples,
         n_features     = n_features,
         centers        = n_centers,
         return_centers = True,
-        random_state   = 42
+        random_state   = 42,
+        cluster_std    = cluster_std
     )
-
-    X, y = load_iris(return_X_y=True)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, 
@@ -36,40 +54,17 @@ def main():
         random_state = 42
     )
 
-    ac_mdl = ACKM(n_clusters = n_centers, max_iter = max_iter)
+    ac_mdl = ACKM(n_clusters = n_centers, max_iter = max_iter, plot=True)
     sk_mdl = SKKM(n_clusters = n_centers, max_iter = max_iter)
 
-    ac_mdl.fit(X_train, y_train)
-    y_pred = ac_mdl.predict(X_test)
-    print("accuracy:", np.sum(y_pred == y_test) / len(y_test))
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=X_test[y_pred == 0, 0], y=X_test[y_pred == 0, 1],
-        mode='markers',marker_color='#DB4CB2',name='Iris-setosa'
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=X_test[y_pred == 1, 0], y=X_test[y_pred == 1, 1],
-        mode='markers',marker_color='#c9e9f6',name='Iris-versicolour'
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=X_test[y_pred == 2, 0], y=X_test[y_pred == 2, 1],
-        mode='markers',marker_color='#7D3AC1',name='Iris-virginica'
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=ac_mdl.centers[:, 0], y=ac_mdl.centers[:,1],
-        mode='markers',marker_color='#CAC9CD',marker_symbol=4,marker_size=13,name='Centroids'
-    ))
-
-    fig.update_layout(template='plotly_dark',width=1000, height=500,)
-    fig.show(renderer='iframe')
+    ac_mdl.fit(X_train)
+    y_pred, centers = ac_mdl.predict(X_test)
+    plot_step(X_test, y_pred, centers)
+    
 
     sk_mdl.fit(X_train, y_train)
     y_pred = sk_mdl.predict(X_test)
-    print("accuracy:", np.sum(y_pred == y_test) / len(y_test))
+    plot_step(X_test, y_pred, sk_mdl.cluster_centers_)
 
 if __name__ == '__main__':
     main()
