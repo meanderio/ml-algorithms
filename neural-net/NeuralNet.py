5190ee78587f1b71a5086910e916a60b536315c2
meanderio <acahn@meanderings.io>
2025-08-27 08:44:13 -0500

    update: solved backprop error for multiclass

--- neural-net/NeuralNet.py
+++ neural-net/NeuralNet.py
@@ -1,17 +1,15 @@
 import numpy as np
-np.random.seed(42)
-import sys
-
-
 class NeuralNetwork:
-    def __init__(self, layers=[], alpha=0.1, n_iters=10_000):
+    def __init__(self, layers=[], alpha=0.1, n_iters=10_000, batch_size=1_000, random_state=42):
         self.layers     = layers
         self.n_layers   = len(layers) - 1 # don't count input
         self.alpha      = alpha
         self.n_iters    = n_iters
+        self.batch_size = batch_size
         self.weights    = self._initialize_weights()
         self.biases     = self._initialize_biases()
         self.costs      = []
+        np.random.seed(random_state)
 
     def _initialize_weights(self,):
         weights = []
@@ -23,27 +21,33 @@ class NeuralNetwork:
     def _initialize_biases(self,):
         biases = []
         for i in range(1, len(self.layers)):
-            #b = np.zeros((self.layers[i],1))
-            b = np.random.randn(self.layers[i],1)
+            b = np.zeros((self.layers[i],1))
             biases.append(b)
         return biases
-
+   
+    def _prep_labels(self, y, n_samples):
+        if len(y.shape) == 1:
+            y = y.reshape(1, n_samples)
+        else:
+            y = y.T
+        return y
+    
     def fit(self, X, y):
-        batch = 1_000
         n_samples, n_features = X.shape
-        y = y.reshape(1, n_samples)
+        # shape labels
+        y = self._prep_labels(y, n_samples)
+        # loop for n epochs
         for _ in range(self.n_iters):
-            for i in range(0,n_samples, batch):
-                x_i = X[i:i+batch,:]
-                y_i = y[:,i:i+batch]
+            # run training samples in batches
+            for i in range(0, n_samples, self.batch_size):
+                # grab x, y batches
+                x_i = X[i:i+self.batch_size,:]
+                y_i = y[:,i:i+self.batch_size]
                 # feed forward to get y_pred
-                #y_hat, A = self.feed_forward(X)
                 y_hat, A = self.feed_forward(x_i)
                 # calculate cost
-                #cost  = self.calculate_cost(y_hat, y)
                 cost  = self.calculate_cost(y_hat, y_i)
                 # backprop to update weights
-                #dws, dbs = self.back_prop(X, y_hat, y, A)
                 dws, dbs = self.back_prop(x_i, y_hat, y_i, A)
                 # update the weights and biases
                 self.gradient_descent(dws, dbs)
@@ -63,11 +67,6 @@ class NeuralNetwork:
 
     def calculate_cost(self, y_hat, y):
         m    = y.shape[1]
-        # argmax if necessary
-        if y_hat.shape[0] != 1:
-            y_hat = np.argmax(y_hat, axis=0)
-            y_hat = y_hat.reshape(1, m)
-        #print(y, y_hat)
         loss = self._rmse(y_hat, y)
         cost = (1 / m) * loss
         self.costs.append(np.sum(cost))
@@ -83,8 +82,6 @@ class NeuralNetwork:
         return A[-1], A
 
     def activation(self, z):
-        #return self._leaky_relu(z)
-        #return self._tanh(z)
         return self._sigmoid(z)
 
     def _leaky_relu(self, z, alpha=0.01):
@@ -103,14 +100,13 @@ class NeuralNetwork:
         dws, dbs = [0] * self.n_layers, [0] * self.n_layers
         m        = y_hat.shape[1]
         dz       = 1
-        t = 0
+        t        = 0
         for i in range(self.n_layers-1,-1,-1):
             # back prop errors
             if t == 0:
                 # output layer diff
-                # THIS NEEDS TO BE FIXED FOR Y DIMS
                 delta = (1/m) * (A[i+1] - y)
-                print(y.shape, A[i+1].shape, delta.shape);sys.exit()
+                #print(y.shape, A[i+1].shape, delta.shape);sys.exit()
             else:
                 # all other layers
                 delta = A[i+1] * (1 - A[i+1])
--- neural-net/main.py
+++ neural-net/main.py
@@ -6,18 +6,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics         import accuracy_score
 
 import tensorflow as tf
-from   tensorflow import keras
+from   tensorflow             import keras
+from   tensorflow.keras.utils import to_categorical
+
 
 import matplotlib.pyplot as plt
 
 def main():
+
+    random_state = 42
     n_samples    = 20_000
     n_features   = 12
     n_redundant  = 0
-    n_classes    = 2
-    random_state = 42
+    n_classes    = 10
     alpha        = 0.1
-    n_iterations = 5_000
+    n_iterations = 100
+    batch_size   = 10_000
     
     """
     X, y = make_classification(
@@ -38,32 +42,48 @@ def main():
 
     X_train = scale(X_train)
     X_test  = scale(X_test)
+
+    layers = [
+        X_train.shape[1], # input layer
+        6,
+        3,
+        1 # output layer
+    ]
     """
+    
     #"""
     # Load the MNIST dataset
     (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
-    X_train = X_train.reshape(60_000, 784)
-    X_test  = X_test.reshape(10_000, 784)
+    X_train                              = X_train.reshape(60_000, 784)
+    X_test                               = X_test.reshape(10_000, 784)
     print(X_train.shape, y_train.shape)
 
     X_train = X_train / 255.
     X_test  = X_test  / 255.
-
-    #X_train = scale(X_train)
-    #X_test  = scale(X_test)
-    #"""
+    y_train = to_categorical(
+        y_train, 
+        num_classes = n_classes
+    )
     
     layers = [
         X_train.shape[1], # input layer
-        8,
-        4,
+        512,
         10 # output layer
     ]
+    #"""
 
-    ac_clf = NeuralNetwork(layers=layers, alpha=alpha, n_iters=n_iterations)
+    ac_clf = NeuralNetwork(
+        layers     = layers, 
+        alpha      = alpha, 
+        n_iters    = n_iterations, 
+        batch_size = batch_size
+    )
+
     ac_clf.fit(X_train, y_train)
     y_pred = ac_clf.predict(X_test).reshape(-1)
     print(f"accuracy: {accuracy_score(y_pred, y_test)}")
+    
+    # plot cost over time
     plt.plot(ac_clf.costs)
     plt.show()
 
